5d9534356af7cfb2cec543961d5186c42898789b
Cristopher Ferrari <ferraricristopher93@gmail.com>
2024-05-13 11:37:30 +0200

    Per Andrea
    Caricamento manuale repo

---
+++ Models/Pizza.cs
@@ -0,0 +1,18 @@
+﻿namespace la_mia_pizzeria_static.Models
+{
+    public class Pizza
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string ImgPath { get; set; }
+        public float Price { get; set; }
+
+        public Pizza(string name, string desc, string imgPath, float price)
+        {
+            Name = name;
+            Description = desc;
+            ImgPath = imgPath;
+            Price = price;
+        }
+    }
+}
